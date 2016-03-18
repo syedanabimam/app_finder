d03cae1194f942f81d47bd7d7b9c0810bc328faa
Syed Anab Imam <syedanabimam@gmail.com>
2016-03-18 03:12:03 +0000

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317203124) do
+ActiveRecord::Schema.define(version: 20160317203145) do
+
+  create_table "pages", force: true do |t|
+    t.string   "search_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "search_id"
